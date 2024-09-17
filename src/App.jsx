ef174c93560bec22fc5ff9a7b8edc978892b1c7b
unknown <amincxo@gmail.com>
2024-09-17 17:09:04 +0330

    install react-roter-dom & config Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import PageNotFund from "./pages/404";
+import CheckoutPage from "./pages/CheckoutPage";
+import DetailsPage from "./pages/DetailsPage";
+import ProductsPage from "./pages/ProductsPage";
+
+import { Routes , Route , Navigate } from "react-router-dom";
 
 
 function App() {
 
 
   return (
-    <>
-     <h1>Amin Shop</h1>
-    </>
-  )
+    <Routes>
+        <Route path="/" element={<Navigate to="/products" replace /> }  />
+        <Route path="/products" element={<ProductsPage/>}  />
+        <Route path="/products/:id" element={<DetailsPage/>}  />
+        <Route path="/checkout" element={<CheckoutPage/>}  />
+        <Route path="/*" element={<PageNotFund/>}  />
+    </Routes>
+  );
 }
 
 export default App
---
+++ src/pages/404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageNotFund () {
+  return (
+    <div> PageNotFund </div>
+  )
+}
+
+export default PageNotFund 
