8270933a6ab88242b7297d8a00c2bdac450f5100
unknown <amincxo@gmail.com>
2024-09-18 02:50:44 +0330

    styles bug fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,8 +8,8 @@ import styles from "./Card.module.css"
 function Card({data}) {
     const {id, title , image , price } = data
   return (
-    <div className={styles.Card} >
-        <img  src={image} alt={title} style={{width: "230px"}} />
+    <div className={styles.card} >
+        <img  src={image} alt={title} />
         <h3>{shortenText(title)}</h3>
         <p> {price} </p>
         <div className={styles.actions} >
