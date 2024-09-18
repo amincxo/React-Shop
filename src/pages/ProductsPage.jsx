4392d504438e7f84af96b61919cd3d9b25cc6114
unknown <amincxo@gmail.com>
2024-09-18 17:41:12 +0330

    create search component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,39 @@
-import { useProducts } from "../context/ProductContext"
-import styles from "./ProductsPage.module.css"
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
+import { useProducts } from "../context/ProductContext"
 
+import styles from "./ProductsPage.module.css"
 
 function ProductsPage() {
     const [products] = useProducts();
+
+    const [search , setSearch] = useState("");
+
+    const searchHandler = () => {
+        console.log(search)
+    }
+
+
   return (
-    <div className={styles.container} >
-        <div className={styles.products} >
-            {products ? products.map((p)=> (
-                <Card key={p.id} data={p} />
-            )) : <Loader/> }
-            
+    <>
+        <div>
+            <input type="text" placeholder="Search ..." value={search} onChange={e => setSearch(e.target.value.toLowerCase().trim())} />
+            <button onClick={searchHandler} >
+                <ImSearch/>
+            </button>
         </div>
-        <div>sidebar</div>
-    </div>
+        <div className={styles.container} >
+            <div className={styles.products} >
+                {products ? products.map((p)=> (
+                    <Card key={p.id} data={p} />
+                )) : <Loader/> }
+                
+            </div>
+            <div>sidebar</div>
+        </div>
+    </>
   )
 }
 
