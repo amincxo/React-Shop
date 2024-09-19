461b6a7da71d24c6e191f1ab46803c0ccbbbe71e
unknown <amincxo@gmail.com>
2024-09-19 20:06:07 +0330

    complate reducers for increse dec checkout basket

--- src/context/CardContext.jsx
+++ src/context/CardContext.jsx
@@ -9,7 +9,6 @@ const initialState = {
 };
 
 const reducer = (state ,action) => {
-    console.log(action);
     switch (action.type) {
         case "ADD_ITEM":
             if(!state.selectedItems.find(item => item.id === action.payload.id)){
@@ -20,6 +19,35 @@ const reducer = (state ,action) => {
                 checkout:false,
                 ...sumProducts(state.selectedItems)
             }
+        case "REMOVE_ITEM":
+            const newSelectecItems = state.selectedItems.filter(item => item.id !== acction.payload.id);
+            return {
+                ...state,
+                selectedItems:[...newSelectecItems],
+                ...sumProducts(newSelectecItems)
+            }
+        case "INCREASE":
+            const increaseIndex = state.selectedItems.findIndex(item => item.id === action.payload.id);
+            state.selectedItems[increaseIndex].quantity++;
+            return {
+                ...state,
+                ...sumProducts(state.selectedItems)
+            }
+        case "DECREASE":
+            const decreaseIndex = state.selectedItems.findIndex(item => item.id === action.payload.id);
+            state.selectedItems[decreaseIndex].quantity--;
+            return {
+                ...state ,
+                ...sumProducts(state.selectedItems),
+            };
+        case "CHECKOUT":
+            return {
+                selectedItems:[],
+                itemsCounter: 0,
+                total:0,
+                checkout:true,
+            }
+
         default:
             throw new Error("Invalid Action!")
     }
